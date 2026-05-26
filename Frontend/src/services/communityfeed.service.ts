0e599086d055793a8dd2a4b76d93fb03964d1c17
agtelo <alantelo1987@gmail.com>
2026-05-26 11:12:07 -0300

    feat: implement backend in CommunityFeed

--- Frontend/src/services/communityfeed.service.ts
+++ Frontend/src/services/communityfeed.service.ts
@@ -1,13 +1,14 @@
 "use server";
 
-import { CommunityFeedI } from "@/interfaces/communityfeed.interface";
+import { Activity, CommunityFeedResponse } from "@/interfaces/activity.interface";
 import axios, { isAxiosError } from "axios";
 import { cookies } from "next/headers";
 
-export const CommunityFeedService = async () => {
+export const CommunityFeedService = async (): Promise<Activity[]> => {
   const cookieStore = await cookies();
+  
   try {
-    const res = await axios<{ feed: CommunityFeedI }>(
+    const res = await axios<CommunityFeedResponse[]>(
       `${process.env.API_URL}/community-feed`,
       {
         headers: {
@@ -15,13 +16,26 @@ export const CommunityFeedService = async () => {
         },
       },
     );
-    console.log(res.data);
+    console.log("Backend response:", res.data);
+    const activities: Activity[] = res.data.map((item, index) => ({
+      id: `${index}`,
+      author: item.user_name, //
+      source: item.channel_name,
+      type: "post", 
+      content: item.content,
+      reactions: item.reactions,
+      replies: 0, //
+      timestamp: item.original_date, // 
+    }));
 
-    return res.data;
+    return activities;
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching community feed:", error);
+    
     if (error && isAxiosError(error)) {
-      throw new Error(error.message);
+      throw new Error(error.response?.data?.message || error.message);
     }
+    
+    throw new Error("Failed to fetch community feed");
   }
 };
